2fb7da369857755761875f2fb011e342b6bbf6df
mbaharip <62494292+mbahArip@users.noreply.github.com>
2023-06-09 20:11:19 +0700

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+const isDev = process.env.NODE_ENV === "development";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://mbaharip.github.io",
-  base: "/acc-model-repo",
+  site: isDev ? undefined : "https://mbaharip.github.io",
+  base: isDev ? undefined : "/acc-model-repo",
 });
